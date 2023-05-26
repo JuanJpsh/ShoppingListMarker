08ed4ebfb67c03e24fe2887e966a427ec1596d6c
JuanJpsh <113944776+JuanJpsh@users.noreply.github.com>
2023-05-25 20:28:31 -0500

    update main with first merge

--- src/app/pages/home/home-page/home.component.ts
+++ src/app/pages/home/home-page/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EmpAddEditComponent } from '../components/emp-add-edit/emp-add-edit.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(private _dialog: MatDialog) {}
 
+  openAddEditEmpForm() {
+    this._dialog.open(EmpAddEditComponent);
+  }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,15 +3,25 @@ import { CommonModule } from '@angular/common';
 import { ListsComponent } from './components/lists/lists.component';
 import { HomeComponent } from './home-page/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { EmpAddEditComponent } from './components/emp-add-edit/emp-add-edit.component';
+import { FormMaterialModule } from 'src/app/commons/modules/form-material.module';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ListsComponent,
-    HomeComponent
+    HomeComponent,
+    EmpAddEditComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormMaterialModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatDialogModule
   ]
 })
 export class HomeModule { }
